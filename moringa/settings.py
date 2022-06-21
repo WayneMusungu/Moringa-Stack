f9e0f658c4cd156ff66b16ebbb04bffe4c0f3034
edah-hub <edah.chepngetich@student.moringaschool.comm>
2022-06-21 09:04:29 +0300

    adds search functionality

--- moringa/settings.py
+++ moringa/settings.py
@@ -9,9 +9,8 @@ https://docs.djangoproject.com/en/4.0/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.0/ref/settings/
 """
-
+import os
 from pathlib import Path
-
 import cloudinary
 import cloudinary.uploader
 import cloudinary.api
@@ -137,6 +136,11 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
 STATIC_URL = 'static/'
+
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, "static"),
+]
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
