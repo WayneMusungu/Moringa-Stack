d23e856245c7aa956c2c412e216f5caf83e9951d
JamesMusembi <james.musembi@student.moringaschool.com>
2022-06-21 11:37:31 +0300

    editing test

--- stack/tests.py
+++ stack/tests.py
@@ -1,3 +1,16 @@
 from django.test import TestCase
+from .models import Question,Comment,Profile
+from django.contrib.auth.models import User
 
 # Create your tests here.
+
+class ProfileTestClass(TestCase):
+    def setUp(self):
+        self.user = User(id=1, username='james', password='1234')
+        self.user.save()
+
+   
+        
+        
+        
+#
