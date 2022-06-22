3b35823d6d2057faebdb7c96846dbdcb34411ee3
JamesMusembi <james.musembi@student.moringaschool.com>
2022-06-22 14:51:43 +0300

    editing README

--- moringa/settings.py
+++ moringa/settings.py
@@ -140,7 +140,9 @@ USE_TZ = True
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = 'static/'
+
 STATIC_FILES_DIRS = [os.path.join(BASE_DIR, 'static')],
+
 MEDIA_URL = 'media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media/')
 
