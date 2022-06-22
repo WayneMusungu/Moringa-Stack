f4f68eb3b0b91ca3600f8579eafb5895c9e6dfe1
Victor Ondari <vic@Victors-MacBook-Pro.local>
2022-06-22 09:33:00 +0300

    added the form for commenting.

--- stack/forms.py
+++ stack/forms.py
@@ -1,8 +1,14 @@
+from dataclasses import field
 from django.contrib.auth.models import User
 from django import forms
-from . models import Profile
+from . models import Profile, Comment
 
 class ProfileForm(forms.ModelForm):
     class Meta:
         model = Profile
         fields = ['profile_picture','bio','email']
+        
+class CommentForm(forms.ModelForm):
+    class Meta:
+        model= Comment
+        fields = ['content']
